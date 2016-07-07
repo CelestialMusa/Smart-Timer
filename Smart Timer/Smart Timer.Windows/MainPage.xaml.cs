2c0b8eb2f5bbf4fa0608234adfb18f29440002f4
CelestialMusa <Zakes.musa@hotmail.com>
2016-07-07 19:24:39 +0200

    Timer Seconds

--- Smart Timer/Smart Timer.Windows/MainPage.xaml.cs
+++ Smart Timer/Smart Timer.Windows/MainPage.xaml.cs
@@ -22,9 +22,39 @@ namespace Smart_Timer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int count = 00;
         public MainPage()
         {
             this.InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.Tick += timer_Tick;
+        }
+
+        private void timer_Tick(object sender, object e)
+        {
+            seconds.Text = count--.ToString();
+
+            if(count ==0)
+            {
+                count = 60;
+                
+                if(timer.Interval.Minutes!=0)
+                {
+                    timer.Interval.Minutes--;
+                }
+            }
+        }
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
